4dcf80f53ff8b30cdab6acf130ab5306ff209d81
Lucas Sato <lucasjks@gmail.com>
2017-12-19 18:52:12 -0800

    Created Board, of which Game is now a Superclass
    Board contains the main game dynamics, and can be used internally in
    algorithms. Board NEVER prints. Game acts as the interactive version of
    Board, printing and taking input.
    
    This distinction is useful because now an internal algorithm can
    experiment freely with the Board class.
    
    Also added unplay_token method in Board (will be used for algorithms).
    
    TODO: include undo_turn functionality for Game on top of
    Board.unplay_token

--- lib/core.py
+++ lib/core.py
@@ -6,7 +6,7 @@ _BOARD_HEIGHT = 7
 _BOARD_WIDTH  = 7
 FOOTER = "   " + "   ".join([str(x+1) for x in range(_BOARD_WIDTH)]) + "\n"
 
-class Game(object):
+class Board:
 
     def __init__(self, height, width):
         self.board = np.full((height, width), "_") 
@@ -16,8 +16,9 @@ class Game(object):
     def __repr__(self):
         return str(self.board)
 
-    def is_over(self):
+    def is_winner(self, token):
         seqs = []
+        is_winner = False
         for i in range(self.height-3): #get all 4-diags
             for j in range(self.width):
                 cur_diag = []
@@ -38,28 +39,46 @@ class Game(object):
             for j in range(self.width):
                 seqs.append((self.board[i:i+4,j]).tolist())
         for seq in seqs:
-            if seq == ["X"]*4:
-                print("X has won the game!")
-                return True
-            if seq == ["O"]*4:
-                print("O has won the game!")
-                return True
-        return False
+            if seq == [token]*4:
+                is_winner = True
+                break
+        return is_winner
         
+    def play_token(self, token, col):
+        for i in range(self.height):
+            if self.board[self.height-i-1][col] == "_":
+                self.board[self.height-i-1][col] = token
+                break
+        else:
+            raise Exception("Column is full!")
+
+    def unplay_token(self, token, col):
+        for i in range(self.height):
+            if self.board[self.height-i-1][col] == token:
+                self.board[self.height-i-1][col] = "_"
+                break
+        else:
+            raise Exception("This token is not the last in this column")
+
+class Game(Board):
+
+    def __init__(self, height, width):
+        super().__init__(height, width)
+        print(str(self) + "\n" + FOOTER)
+
+    def __repr__(self):
+        return str(self.board)
+
     def play_turns(self):
         for token in ["X", "O"]:
-            print(str(self) + "\n" + FOOTER)
-            if self.is_over():
-                return False
-            print("It's {}'s turn. Which column do you want to play?".format(token))
+            print("It's {}'s turn. Which column do you want to play? Enter 0 to undo last move.".format(token))
             col = int(input()) - 1
-            print("\n")
-            for i in range(self.height):
-                if self.board[self.height-i-1][col] == "_":
-                    self.board[self.height-i-1][col] = token
-                    break
-            else:
-                raise Exception("Column is full!")
+            self.play_token(token, col)
+            print("\n" + str(self) + "\n" + FOOTER)
+            if self.is_winner(token):
+                print("{} has won the game!".format(token))
+                return False
+            last_play_col = col
         return True
 
 def main():
